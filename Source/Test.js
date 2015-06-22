923802ef232982bc2ed041e2afec9dae0efca037
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-21 22:57:32 -0600

    :new: Add Tests

---
+++ Source/Test.js
@@ -0,0 +1,27 @@
+"use strict"
+let Serialize = require('./Main')
+let Assert = require('assert')
+
+Assert.equal(
+  Serialize.unserialize(Serialize.serialize(1)), 1, "Works with integers"
+)
+
+Assert.equal(
+  Serialize.unserialize(Serialize.serialize(true)), true, "Works with booleans"
+)
+
+Assert.equal(
+  Serialize.unserialize(Serialize.serialize(1.1)), 1.1, "Works with Floats"
+)
+
+Assert.equal(
+  Serialize.unserialize(Serialize.serialize("a")), "a", "Works with Strings"
+)
+
+Assert.equal(
+  Serialize.unserialize(Serialize.serialize(null)), null, "Works with null"
+)
+
+Assert.deepEqual(
+  Serialize.unserialize(Serialize.serialize({a: "a"})), {a: "a"}, "Works with Objects"
+)
