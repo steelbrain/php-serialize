74c5aa8e6c4bce5340333d7d1e3a121931844233
Steel Brain <i@steelbrain.me>
2018-08-14 03:21:36 -0700

    :arrow_up: Upgrade dependencies and circleci config

--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,6 @@
-/* @flow */
+// @flow
 
-/* eslint-disable camelcase */
-
-class __PHP_Incomplete_Class {
-  __PHP_Incomplete_Class_Name: string;
-
-  constructor(name: string) {
-    this.__PHP_Incomplete_Class_Name = name
-  }
-}
-
-export function getClass(prototype: Object) {
-  function Container() { }
-  Container.prototype = prototype
-  return Container
-}
-
-export function getIncompleteClass(name: string) {
-  return new __PHP_Incomplete_Class(name)
-}
+/* eslint-disable camelcase,import/prefer-default-export */
 
 export function getByteLength(contents: string): number {
   if (typeof Buffer !== 'undefined') {
