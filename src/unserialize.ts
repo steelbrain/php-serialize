bf0a444dc42da4643674af84b0ee1fe53dac98b6
Roman Pogorelchuk <roman.pogorelchuk@namecheap.com>
2025-03-01 23:20:48 +0200

    Fix: SyntaxError: Cannot convert 79.950000000000003 to a BigInt

--- src/unserialize.ts
+++ src/unserialize.ts
@@ -47,8 +47,12 @@ function unserializeItem(parser: Parser, scope: Record<string, any>, options: Op
   if (type === 'int' || type === 'float') {
     const value = parser.readUntil(';')
     let parsedValue: number | BigInt = type === 'int' ? parseInt(value, 10) : parseFloat(value)
-    if (parsedValue.toString() !== value) {
-      parsedValue = BigInt(value)
+    if (type === 'int') {
+      if (!value.includes('.')) {
+        parsedValue = BigInt(value); // Only convert to BigInt if there's no decimal
+      } else {
+        parsedValue = parseFloat(value); // Ensure floats remain as float
+      }
     }
     return parsedValue
   }
